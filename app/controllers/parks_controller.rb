e40e977b049f339fb6ebe347626d40b6f8cab79d
spark <spark@MacBook-Air-2.local>
2016-11-01 13:29:03 -0700

    Add incomplete forms for parks and reviews

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -5,6 +5,58 @@ class ParksController < ApplicationController
   end
 
   def show
+    @city = City.find(params[:city_id])
+    @park = Park.find(params[:id])
+  end
+
+  def new
+    @city = City.find(params[:city_id])
+    @park = Park.new
+  end
+
+  def edit
+    @city = City.find(params[:city_id])
+    @park = Park.find(params[:id])
+  end
+
+  def create
+    @city = City.find(params[:city_id])
+    @park = Park.new
+    @park.assign_attributes(park_params)
+    @park.city = @city
+
+    if @park.save
+      # redirect to the park show
+      # redirect_to ...
+    else
+      @errors = @park.errors.full_messages
+      # render the new page again
+    end
+  end
+
+  def update
+    @city = City.find(params[:city_id])
+    @park = Park.find(params[:id])
+    if @park.update(park_params)
+      # redirect to the park show
+      # redirect_to ...
+    else
+      @errors = @park.errors.full_messages
+      # render the edit page again
+      # render ...
+    end
+  end
+
+  private
+
+  def park_params
+    params.require(:park).permit(:name, :description, :city)
+  end
+
+  # being dry
+
+  def set_city_and_park
+    @city = City.find(params[:city_id])
     @park = Park.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :cities, only: :index do
-    resources :parks, only: [:index, :show]
+    resources :parks, only: [:index, :show, :new, :edit] do
+      resources :reviews, except: :index
+    end
   end
 end
