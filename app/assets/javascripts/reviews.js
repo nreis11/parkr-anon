d11f807f556c878447d8ff09a8334eea684b6394
reeses-pieces <nicholasreis@icloud.com>
2016-11-01 22:39:55 -0700

    allow for multiple new review ajax calls

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -9,13 +9,16 @@ $(document).ready(function(){
   $('.new_review_link').on('ajax:success', function(event, response){
     console.log(event);
     console.log(response);
-  
-    $('#form-container').append(response);
+    
+    $('.new_review_link').hide()
+    $('#form-container').html(response);
   });
 
   $('#form-container').on('ajax:success', '#new_review', function(event, response){
     console.log("we gucci?");
     console.log(response);
+    $('#form-container').empty()
+    $('.new_review_link').show()
   })
 
   // $('#new_review').on('ajax:success', function(event, response){
